4f35d8d325e7cd268bcfae375ea338c0818e70d2
kon9397 <40241963+kon9397@users.noreply.github.com>
2019-08-11 10:54:53 +0300

    Add files

---
+++ js/main.js
@@ -0,0 +1,71 @@
+$('.slider').slick({
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 3,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      }
+    ]
+  });
+
+$('.slider-wrapper').slick({
+  // slidesToShow: 5,
+  // slidesToScroll: 1,
+  // autoplay: false,
+  // autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 9999,
+      settings: "unslick"
+    },
+    {
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true
+      }
+    }
+  ]
+});
+
+$('[data-modal]').click(function(e) {
+    let target = this.getAttribute('data-modal');
+    
+    $(`.card${target}`).addClass('active');
+    $(`.card${target} .main-content`).removeClass('active');
+    $(`.card${target} .changed-content`).addClass('active');
+
+    if (e.target.getAttribute('data-modal') === 'close') {
+        $(`.card${target}`).removeClass('active');
+        $(`.card${target} .main-content`).addClass('active');
+        $(`.card${target} .changed-content`).removeClass('active');
+    };
+})
